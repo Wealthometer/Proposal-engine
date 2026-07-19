8a9cd31e20f809f3fe0ce3f827185aa37a4ffd1b
Wealthometer <wealthhajoh87@gmail.com>
2026-07-19 10:33:05 +0100

    Update EventController.java

--- backend/affection-event-service/src/main/java/com/love/events/controller/EventController.java
+++ backend/affection-event-service/src/main/java/com/love/events/controller/EventController.java
@@ -5,3 +5,4 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
