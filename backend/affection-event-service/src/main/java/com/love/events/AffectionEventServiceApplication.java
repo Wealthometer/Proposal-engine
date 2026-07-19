2dc8fc5074c5b205f846e526f3e93aa845dfb9ac
Wealthometer <wealthhajoh87@gmail.com>
2026-07-19 10:32:28 +0100

    Update AffectionEventServiceApplication.java

--- backend/affection-event-service/src/main/java/com/love/events/AffectionEventServiceApplication.java
+++ backend/affection-event-service/src/main/java/com/love/events/AffectionEventServiceApplication.java
@@ -3,3 +3,4 @@ package com.love.events;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
