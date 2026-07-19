fab716acabf7f4f15a61214213ed3a3e6befbf3b
Wealthometer <wealthhajoh87@gmail.com>
2026-07-19 10:35:56 +0100

    Update AffectionEventConsumer.java

--- backend/affection-event-service/src/main/java/com/love/events/service/AffectionEventConsumer.java
+++ backend/affection-event-service/src/main/java/com/love/events/service/AffectionEventConsumer.java
@@ -5,5 +5,6 @@ import com.love.events.dto.EventView;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
 @Slf4j
