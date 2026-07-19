4e70473539066a703aa0476a97d64b50f593ee60
Wealthometer <wealthhajoh87@gmail.com>
2026-07-19 10:36:17 +0100

    Update AffectionEventConsumer.java

--- backend/affection-event-service/src/main/java/com/love/events/service/AffectionEventConsumer.java
+++ backend/affection-event-service/src/main/java/com/love/events/service/AffectionEventConsumer.java
@@ -17,3 +17,4 @@ public class AffectionEventConsumer {
     private final StringRedisTemplate redis;
     private final ObjectMapper objectMapper;
 
+    public AffectionEventConsumer(StringRedisTemplate redis, ObjectMapper objectMapper) {
