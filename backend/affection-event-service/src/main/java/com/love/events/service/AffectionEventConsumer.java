cadcd8e03fd136af18dc3c376f8c01e894e57f80
Wealthometer <wealthhajoh87@gmail.com>
2026-07-19 10:35:45 +0100

    Update AffectionEventConsumer.java

--- backend/affection-event-service/src/main/java/com/love/events/service/AffectionEventConsumer.java
+++ backend/affection-event-service/src/main/java/com/love/events/service/AffectionEventConsumer.java
@@ -3,3 +3,4 @@ package com.love.events.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.love.events.dto.EventView;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
