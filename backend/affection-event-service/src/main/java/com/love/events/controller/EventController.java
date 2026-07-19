d071564736c538739b227cc31bad4d2702febfca
Wealthometer <wealthhajoh87@gmail.com>
2026-07-19 10:33:02 +0100

    Update EventController.java

--- backend/affection-event-service/src/main/java/com/love/events/controller/EventController.java
+++ backend/affection-event-service/src/main/java/com/love/events/controller/EventController.java
@@ -3,3 +3,4 @@ package com.love.events.controller;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
